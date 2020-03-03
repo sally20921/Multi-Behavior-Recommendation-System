e0154ae09f2ec99a3169b05874f0fca121d2363c
Seri Lee <sally20921@snu.ac.kr>
2020-03-03 16:50:44 +0900

    Update modules.py

--- modules.py
+++ modules.py
@@ -2,5 +2,18 @@ import numpy as np
 import tensorflow as tf
 
 
+'''ln: layer normalization.'''
+'''get_token_embeddings: construct  token embedding matrix. 
+returns weight variable (V, E)'''
+'''scaled_dot_product_attention: ''''
+'''mask: mask paddings on keys or queries to inputs'''
+'''multihead_attention: '''
+'''ff: position-wise feed-forwared network.'''
+
+'''label_smoothing: applies label smoothing.
+returns 3d tensor that has the same shape as inputs '''
+
+
+
                         
                      
