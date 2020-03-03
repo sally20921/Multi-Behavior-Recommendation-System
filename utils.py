c8dba8f1638df71014e80d7eec6a8185c1a2d402
Seri Lee <sally20921@snu.ac.kr>
2020-03-03 17:27:59 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,12 @@
 '''utility functinons'''
+import tensorflow as tf
+import  json 
+import os, re
+import logging  
+
+logging.basicConfig(level=logging.INFO)
+
+def calc_num_batches(total_num, batch_size):
+def convert_idx_to_token_tensor(inputs, idx2token):
+def postprocess(hypotheses, idx2token):
+  
