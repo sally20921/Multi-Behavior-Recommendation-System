f8df1f31888c2c8d0d64645c16d77ba6419b975a
Seri Lee <sally20921@snu.ac.kr>
2020-03-03 14:35:53 +0900

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,14 +1,6 @@
 import numpy as np
 import tensorflow as tf
 
-def scaled_dot_product_attention(Q, K, V, key_masks, 
-                                causality=False, dropout_rate = 0.,
-                                training=True, scope="scaled_dot_product_attention"):
-                                
-                                
-def multihead_attention(queries, keys, values, key_masks, 
-                        num_heads=8, dropout_rate=0,
-                        training=True, causality=False, 
-                        scope="multihead_attention");
+
                         
                      
