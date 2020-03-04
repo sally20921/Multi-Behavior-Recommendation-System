739b1eff23705b50098fba3b9a5417d84f854534
Seri Lee <sally20921@snu.ac.kr>
2020-03-04 21:35:18 +0900

    Delete input.py

--- input.py
+++
@@ -1,8 +0,0 @@
-import numpy as np 
-
-class DataInput: 
-  def __init__(self, data, batch_size):
-    self.batch_size =  batch_size
-    self.data = data 
-    self.epoch_size = len(self.data)//self.batch_size
-                  
